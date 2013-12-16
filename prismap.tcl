fecce68a5679142140793b0120570eae19ba55ef
Jim DeVona <jim.devona@gmail.com>
2013-12-15 22:45:00 -0500

    Refined comments and error messages

--- prismap.tcl
+++ prismap.tcl
@@ -5,14 +5,33 @@ package require msgcat
 
 ::msgcat::mcload [file join [file dirname [info script]] {msgs}]
 
-# Populates shp() array with shapefile info. shp keys:
-# file - shapefile token
-# count - entities in shapefile
-# xmin, xmax, ymin, ymax - bounding box extents
-# x_offset, y_offset - offset to bounding box centroid
-# x_size, y_size - bounding box dimensions
-# attr - id of extrusion attribute
-# min, max - minimum and maximum values of attr
+proc Prismap {arglist} {
+	global config
+	
+	# creates initial config array
+	ConfigDefaults
+	
+	# parses option and updates config
+	ConfigOptions $arglist
+	
+	# reads shapefile and creates shp array
+	OpenShapefile $config(in) $config(attr)
+	
+	# finalize config values that depend on shp data
+	ConfigCheck
+	
+	ConfigLog
+	OpenOutput $config(out)
+	
+	# process each feature in shapefile,
+	# generating an OpenSCAD polygon outline from its geometry
+	# and extruded according to its attribute value
+	Process
+	
+	CloseOutput
+	CloseShapefile
+}
+
 proc OpenShapefile {path attr_name} {
 	global shp
 	
@@ -24,27 +43,18 @@ proc OpenShapefile {path attr_name} {
 		Abort {Only polygon shapefiles are supported.}
 	}
 	
+	# count - number of features in shapefile
 	set shp(count) [$shp(file) info count]
 	if {$shp(count) < 1} {
-		Abort "Shapefile contains no features."
+		Abort {Shapefile contains no features.}
 	}
-	
-	lassign [$shp(file) info bounds] shp(xmin) shp(ymin) shp(xmax) shp(ymax)
-	
-	# translation to move bounding box to origin
-	set shp(x_offset) [expr {($shp(xmax) + $shp(xmin)) / -2.0}]
-	set shp(y_offset) [expr {($shp(ymax) + $shp(ymin)) / -2.0}]
-	
-	# bounding box dimensions
-	set shp(x_size) [expr {$shp(xmax) - $shp(xmin)}]
-	set shp(y_size) [expr {$shp(ymax) - $shp(ymin)}]
-	
-	# index of named attribute
+
+	# attr - index of named attribute
 	if {[catch {$shp(file) fields index $attr_name} shp(attr)]} {
 		Abort $shp(attr)
 	}
 	
-	# get attribute min/max values (initialize to first)
+	# min, max - attribute bounds
 	set shp(min) [set shp(max) [$shp(file) attribute read 0 $shp(attr)]]
 	for {set i 1} {$i < $shp(count)} {incr i} {
 		set value [$shp(file) attribute read $i $shp(attr)]
@@ -55,6 +65,18 @@ proc OpenShapefile {path attr_name} {
 			set shp(max) $value
 		}
 	}
+	
+	# xmin, xmax, ymin, ymax - geometry bounding box
+	lassign [$shp(file) info bounds] shp(xmin) shp(ymin) shp(xmax) shp(ymax)
+	
+	# x_offset, y_offset - translation from bounding box center to origin
+	set shp(x_offset) [expr {($shp(xmax) + $shp(xmin)) / -2.0}]
+	set shp(y_offset) [expr {($shp(ymax) + $shp(ymin)) / -2.0}]
+	
+	# x_size, y_size - bounding box dimensions
+	set shp(x_size) [expr {$shp(xmax) - $shp(xmin)}]
+	set shp(y_size) [expr {$shp(ymax) - $shp(ymin)}]
+	
 }
 
 proc CloseShapefile {} {
@@ -83,35 +105,50 @@ proc Output {msg args} {
 	puts $out [format $msg {*}$args]
 }
 
-# xs ys zs - size
-# x y z - location of origin corner
 proc OutputCube {xs ys zs x y z} {
-	Output "translate(\[%f, %f, %f\])" $x $y $z
-	Output "cube(\[%f, %f, %f\]);" $xs $ys $zs
+	# x, y, z - position of cube corner
+	Output {translate([%f, %f, %f])} $x $y $z
+	# xs, ys, zs - cube size
+	Output {cube([%f, %f, %f]);} $xs $ys $zs
 }
 
-# return a tuple of point list and part point index list
 proc ReformatCoords {coords} {
 	set points [list]
-	set parts [list]
+	set paths [list]
 	set index 0
+	
+	# coords is a list of one or more coordinate lists,
+	# each representing part of a single polygon feature
 	foreach part $coords {
-		set part_points [list]		
+		set part_path [list]
+		
+		# each part list consists of a series of x y vertex coordinates.
+		# the last vertex repeats the first and can be ignored.
 		foreach {x y} [lrange $part 0 end-2] {
-			lappend points [format "\[%s, %s\]" $x $y]
-			lappend part_points $index
+			
+			# vertices of all parts in the feature are accumulated in a single list...
+			lappend points [format {[%s, %s]} $x $y]
+			
+			# ... but the indices of the vertices that make up each part are grouped...
+			lappend part_path $index
 			incr index
 		}
-		lappend parts [format "\[%s\]" [join $part_points ","]]
+		
+		# ... and each part's path is appended to the feature's path list when complete.
+		lappend paths [format {[%s]} [join $part_path ","]]
 	}
-	return [list [join $points ",\n"] [join $parts ",\n"]]
+	
+	# return tuple of OpenSCAD polygon() points= and paths= values.
+	return [list [join $points ",\n"] [join $paths ",\n"]]
 }
 
 proc Floor {} {
 	global config
 	global shp
 	if {$config(floor) != 0} {
-		Output "// Floor:"
+		Output {// Floor:}
+		# the floor's xy dimensions are the same as the map bounds;
+		# floor is positioned under map by translation to bounds min  
 		OutputCube \
 				$shp(x_size) $shp(y_size) $config(base) \
 				$shp(xmin) $shp(ymin) 0
@@ -122,8 +159,8 @@ proc Walls {} {
 	global config
 	global shp
 	if {$config(walls) != 0} {
-		Output "// Walls:"
-		# "west" wall (extends north to line up with north wall)
+		Output {// Walls:}
+		# "west" wall (y size extended by wall width to make a clean corner w/north wall)
 		OutputCube \
 				$config(walls) [expr {$shp(y_size) + $config(walls)}] $config(height) \
 				[expr {$shp(xmin) - $config(walls)}] $shp(ymin) 0
@@ -136,15 +173,19 @@ proc Walls {} {
 
 proc ExtrusionHeight {measure} {
 	global config
+	#   (measure - lower) yields data height in attribute units
+	#  ((measure - lower) * scale) yields data height in model units
+	# (((measure - lower) * scale) + base) yields total height in model units
 	return [expr {$config(base) + ($config(scale) * (double($measure) - $config(lower)))}]
 }
 
 proc Process {} {
 	global shp
 	
-	# combine everything and move it to the origin
-	Output "union() {translate(\[%s, %s, 0\]) {" $shp(x_offset) $shp(y_offset)
+	# everything is unioned and relocated to the model origin
+	Output "union() {\ntranslate(\[%s, %s, 0\]) {" $shp(x_offset) $shp(y_offset)
 	
+	# floor and wall surfaces will be generated if configured
 	Floor
 	Walls
 	
@@ -157,9 +198,8 @@ proc Process {} {
 		# get coordinates; may consist of multiple rings
 		lassign [ReformatCoords [$shp(file) coordinates read $i]] points parts
 		
-		# Fortunately, OpenSCAD seems to be able to sort out islands and holes itself,
-		# so all we need to do is reformat the coords lists as a single points list
-		# and an associated parts points index list.
+		# Fortunately, OpenSCAD is able to sort out islands and holes itself,
+		# so we don't really need to perform any analysis of the coordinates.
 		Output "// Feature: %d, Value: %s, Parts: %d" $i $measure [llength $parts]
 		Output "linear_extrude(height=%f)" $extrusion
 		Output "polygon(points=\[\n%s\n\], paths=\[\n%s\n\]);" $points $parts
@@ -194,10 +234,13 @@ proc ConfigOptions {argl} {
 		switch -- $arg {
 			-b - --base {
 				if {[scan [lindex $argl [incr a]] %f config(base)] != 1} {
-					Abort {%1$s must be numeric.} $arg
+					Abort {Base thickness must be numeric.}
 				}
+				# Arbitrary nonzero minimum base height ensures at least
+				# something is printed for features at lower bound of data...
+				# and avoids weird results with 0 linear_extrude height.
 				if {$config(base) < 0.1} {
-					Abort {%1$s must be >= 0.1 (%2$s)} $arg $config(base)
+					Abort {Base thickness must be >= 0.1 (%2$s).} $config(base)
 				}
 			}
 			-f - --floor {
@@ -205,55 +248,57 @@ proc ConfigOptions {argl} {
 			}
 			-w - --walls {
 				if {[scan [lindex $argl [incr a]] %f config(walls)] != 1} {
-					Abort {%1$s must be numeric.} $arg
+					Abort {Wall thickness must be numeric.}
 				}
+				# Minimum wall thickness set to match minimum base.
 				if {$config(walls) < 0.1} {
-					Abort {%1$s must be >= 0.1 (%2$s)} $arg $config(walls)
+					Abort {Wall thickness must be >= 0.1 (%2$s).} $config(walls)
 				}
 				# walls require floor
 				set config(floor) 1
 			}
 			-l - --lower {
 				if {[scan [lindex $argl [incr a]] %f config(lower)] != 1} {
-					Abort {%1$s must be numeric.} $arg
+					Abort {Lower bound value must be numeric.}
 				}
 			}
 			-u - --upper {
 				if {[scan [lindex $argl [incr a]] %f config(upper)] != 1} {
-					Abort {%1$s must be numeric.} $arg
+					Abort {Upper bound value must be numeric.}
 				}
 			}
 			-h - --height {
+				# height overrides scale even if scale is also specified
 				if {[scan [lindex $argl [incr a]] %f config(height)] != 1} {
-					Abort {%1$s must be numeric.} $arg
+					Abort {Height must be numeric.}
 				}
 				if {$config(height) <= 0} {
-					Abort {%1$s must be > 0. (%2$s)} $arg $config(height)
+					Abort {Height must be > 0 (%2$s).} $config(height)
 				}
 			}
 			-s - --scale {
 				if {[scan [lindex $argl [incr a]] %f config(scale)] != 1} {
-					Abort {%1$s must be numeric.} $arg
+					Abort {Scale must be numeric.}
 				}
 				if {$config(scale) <= 0} {
-					Abort {%1$s must be > 0. (%2$s)} $arg $config(scale)
+					Abort {Scale must be > 0 (%2$s).} $config(scale)
 				}
 			}
 			-i - --in {
 				if {$config(in) ne {}} {
-					Abort {--in already set.}
+					Abort {Input path set multiple times.}
 				}
 				set config(in) [lindex $argl [incr a]]
 			}
 			-a - --attribute {
 				if {$config(attr) ne {}} {
-					Abort {--attribute already set.}
+					Abort {Attribute name set multiple times.}
 				}
 				set config(attr) [lindex $argl [incr a]]
 			}
 			-o - --out {
 				if {$config(out) ne {}} {
-					Abort {--out already set.}
+					Abort {Output path set multiple times.}
 				}
 				set config(out) [lindex $argl [incr a]]
 			}
@@ -265,20 +310,20 @@ proc ConfigOptions {argl} {
 				exit 0
 			}
 			default {
-				Abort {unrecognized option %1$s} $arg
+				Abort {Unrecognized option: %1$s} $arg
 			}
 		}
 	}
 	
 	# check for required arguments
 	if {$config(in) == {}} {
-		Abort {--in shapefile must be specified.}
+		Abort {Shapefile path must be specified with -i or --in.}
 	}
 	if {$config(attr) == {}} {
-		Abort {-a attribute must be specified.}
+		Abort {Attribute name must be specified with -a or --attribute.}
 	}
 	if {$config(out) == {}} {
-		Abort {--out scad file must be specified.}
+		Abort {Output path must be specified with -o or --output.}
 	}
 }
 
@@ -288,19 +333,23 @@ proc ConfigCheck {} {
 	global shp
 	
 	if {$config(lower) == {}} {
+		# default lower bound of extrusion is lower bound of data
 		set config(lower) $shp(min)
 	} elseif {$config(lower) > $shp(min)} {
-		Abort {--lower must be <= --attr minimum (%1$s > %2$s)} $config(lower) $shp(min)
+		# if lower bound of extrusion is explicitly set, it must not be greater that lower bound of data
+		Abort {Lower bound value (%1$s) must be <= minimum attribute value (%2$s).} $config(lower) $shp(min)
 	}
 	
 	if {$config(upper) == {}} {
+		# default upper bound of extrusion is upper bound of data
 		set config(upper) $shp(max)
 	} elseif {$config(upper) < $shp(max)} {
-		Abort {--to must be >= --attr maximum (%1$s < %2$s)} $config(upper) $shp(max)
+		# if upper bound of extrusion is explicitly set, it must not be lower than upper bound of data
+		Abort {Upper bound value (%1$s) must be >= maximum attribute value (%2$s).} $config(upper) $shp(max)
 	}
 	
-	# if height is set, it is used to compute the scale
-	# such that --to would be extruded to height + base.
+	# if height is set, it is used to compute the scale such that extruded height of a
+	# value at the upper bound of data would be exactly at the upper bound of extrusion 
 	if {$config(height) ne {}} {
 		set config(scale) [expr {($config(height) - $config(base)) / ($config(upper) - $config(lower))}]
 	} else {
@@ -311,6 +360,7 @@ proc ConfigCheck {} {
 proc ConfigLog {} {
 	global config
 	if {$config(verbose)} {
+		# unordered report of config values
 		foreach {option value} [array get config] {
 			Log "$option: $value"
 		}
@@ -331,15 +381,4 @@ proc Abort {msg args} {
 	exit 1
 }
 
-
-
-
-ConfigDefaults
-ConfigOptions $::argv
-OpenShapefile $config(in) $config(attr)
-ConfigCheck
-ConfigLog
-OpenOutput $config(out)
-Process
-CloseOutput
-CloseShapefile
+Prismap $argv
